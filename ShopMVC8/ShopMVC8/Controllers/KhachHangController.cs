3eafdc2ef1526904a59e5c82c7745d850d3398e9
bach04py <ishamelforme@gmail.com>
2024-01-03 22:07:59 +0700

    thêm idex khách hàng

--- ShopMVC8/ShopMVC8/Controllers/KhachHangController.cs
+++ ShopMVC8/ShopMVC8/Controllers/KhachHangController.cs
@@ -136,6 +136,17 @@ namespace ShopMVC8.Controllers
      // Ví dụ: return RedirectToAction("Login", "Account");
         return RedirectToAction("Index", "Home");
         }
+        [HttpGet]
+        [Route("admin/khachhang/index")]
+        public IActionResult Index()
+        {
+            // Truy vấn tất cả khách hàng từ cơ sở dữ liệu
+            var allKhachHang = db.KhachHangs.ToList();
+
+            // Truyền danh sách khách hàng đến view để hiển thị
+            return View(allKhachHang);
+        }
+
     }
 }
         
